015237d09ff74076b5096d486d3fdb5810d93f81
dmitry sevostyanov <dsevosty@dev.null>
2018-11-18 11:51:23 +0300

    work commit

--- src/main/java/com/redhat/dsevosty/backend/account/model/AccountDataObject.java
+++ src/main/java/com/redhat/dsevosty/backend/account/model/AccountDataObject.java
@@ -140,7 +140,7 @@ public class AccountDataObject implements AbstractDataObject {
     }
 
     public String toString() {
-        return toStringAbstract();
+        return toString();
     }
 
     @Override
